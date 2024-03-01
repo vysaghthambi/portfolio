a215ce1300e4608b1dc146d1453d794c8a68c5e1
Vysagh K T <vysagh.kt@gadgeon.com>
2024-03-01 12:46:58 +0530

    chore: remove presets in release file

--- release.config.cjs
+++ release.config.cjs
@@ -1,15 +1,9 @@
 const config = {
 	branches: ["main"],
 	plugins: [
-		["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
-		["@semantic-release/release-notes-generator", { preset: "conventionalcommits" }],
-		[
-			"@semantic-release/changelog",
-			{
-				changelogTitle:
-					"# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.",
-			},
-		],
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
 		"@semantic-release/git",
 	],
 };
