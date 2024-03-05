0f39779385ebc8914c4698190ae95a45a4df144e
Vysagh K T <vysagh.kt@gadgeon.com>
2024-03-05 19:23:50 +0530

    style(home): rewamp home page hero section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,36 +1,31 @@
-import AboutIntro from "@/components/AboutIntro/AboutIntro";
-import LinkButton from "@/components/common/LinkButton/LinkButton";
-
-import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 import style from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <Grid container className={style.HeroSection}>
-        <Grid item md={6} display="flex" justifyContent="center" alignItems="center">
-          <Stack>
-            <Typography fontSize="1.4rem" fontWeight={400} letterSpacing=".2rem" color="info">
-              THIS IS ME
-            </Typography>
-            <Typography fontSize="6rem" fontWeight={600}>
-              VYSAGH K T
-            </Typography>
-            <Typography fontSize="2rem" fontWeight={400} letterSpacing=".2rem">
-              Software Engineer
-            </Typography>
-          </Stack>
-        </Grid>
-        <Grid item md={6}></Grid>
-      </Grid>
-
-      <Container maxWidth="lg">
-        <AboutIntro viewMoreComponent={<LinkButton href="/about" />} />
-      </Container>
-    </>
+    <Box className={style.HeroSection}>
+      <div className={style.TextContainer}>
+        <Typography fontSize="2rem" fontWeight={400} letterSpacing=".2rem" color="text.primary">
+          THIS IS ME
+        </Typography>
+        <Typography fontSize="4.8rem" fontWeight={800} color="text.secondary">
+          VYSAGH K T
+        </Typography>
+        <Typography fontSize="4.8rem" fontWeight={800} color="text.secondary">
+          SOFTWARE ENGINEER
+        </Typography>
+        <div>
+          <ArrowDownwardIcon fontSize="large" />
+          <Typography fontSize="1.6rem" color="text.secondary" ml={1}>
+            Scroll
+          </Typography>
+        </div>
+      </div>
+      <Box className={style.HeroSectionFooter} />
+    </Box>
   );
 }
